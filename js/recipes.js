3d76909f0a2c6face185d1253a824585fccdae46
Amy Tennant <amy.tennant88@gmail.com>
2017-10-13 12:53:11 -0600

    Add the ability to add multiple recipes. Refactor the adding ingredients to use one function. Created an initalise function that is called when a new recipe is added

--- js/recipes.js
+++ js/recipes.js
@@ -1,49 +1,84 @@
 
-
-//Add ingredient
-$(".fa-plus").on("click", addIngredient);
-$("input[placeholder='Unit']").keypress(addIngredientWithEnter);
-$("input[placeholder='Ingredient']").keypress(addIngredientWithEnter);
-
-function addIngredientWithEnter(event)
+function initalise()
 {
-	//When enter pushed
-	if(event.which === 13)
-	{
-		addIngredient();
-	}	
+	//Add ingredient
+	$(".add").on("click", ".fa-plus", addIngredient);
+	$("input[placeholder='Unit']").keypress(addIngredient);
+	$("input[placeholder='Ingredient']").keypress(addIngredient);
+
+	//Delete ingredient
+	$("tbody").on("click", ".fa-trash-o", deleteIngredient);
+
+	//Change table entry to input when clicked
+	$("tbody").on("click", ".editable", tableEntryToInput);
+
+	//Stop input click triggering td click
+	$("tbody").on("click", "input", function(event){
+		event.stopPropagation();
+	});
+
+	//Save ingredient changes to table
+	$("tbody").on("change", "input", saveIngredientChanges);
+	$("tbody").on("blur", "input", saveIngredientChanges);
 }
 
+
+//Add recipe
+$(".addRecipe").on("click", ".fa-plus", addRecipe);
+
+function addRecipe()
+{
+	var recipeHtml = "<div class='col-lg-4 col-sm-6'>" +
+									   "<div class='recipe'>" + 
+			  						   "<input class='recipeName' type='text' placeholder='Name'>" +
+											   "<table class='table'>" +
+												   "<thead>" +
+													   "<tr>" +
+														   "<th class='unit'><input type='text' placeholder='Unit'></th>" +
+															 "<th class='ingredient'><input type='text' placeholder='Ingredient'></th>" +
+															 "<th class='add'><i class='fa fa-plus'></i></th>" +
+														 "</tr>" +
+													 "</thead>" +
+													 "<tbody>" +
+													 "</tbody>" +
+												 "</table>" +
+										 "</div>" +
+									 "</div>"
+
+	$(".row").append(recipeHtml);
+
+	initalise();
+}
+
+
+//Add ingredient
 function addIngredient()
 {
-	//Get unit and ingredients inputs
-	var unitText = $("input[placeholder='Unit']").val();
-	var ingredientText = $("input[placeholder='Ingredient']").val();
-
-	if(unitText && ingredientText)
+	//Only add if plus clicked or enter pressed
+	if(event.which === 13 || event.which === 1)
 	{
-		//Add unit and ingredient to recipe
+		//Get unit and ingredients inputs
+		var unitText = $(this).closest("tr").find("input[placeholder='Unit']").val();
+		var ingredientText = $(this).closest("tr").find("input[placeholder='Ingredient']").val();
 
-		// var row = "<tr><td><input type='text' value='" + unitText + 
-		// 					"'></td><td><input type='text' value='" + ingredientText + 
-		// 					"'></td><td class='delete'><i class='fa fa-trash-o'></i></td></tr>"
+		if(unitText && ingredientText)
+		{
+			//Add unit and ingredient to recipe
+			var row = "<tr><td class='editable'>" + unitText + 
+			          "</td><td class='editable'>" + ingredientText + 
+								"</td><td class='delete'><i class='fa fa-trash-o'></i></td></tr>"
 
-		var row = "<tr><td>" + unitText + "</td><td>" + ingredientText + 
-							"</td><td class='delete'><i class='fa fa-trash-o'></i></td></tr>"
+			$(this).closest(".table").append(row);
 
-		$(".table").append(row);
-
-		//Reset unit and ingredient inputs
-		$("input[placeholder='Unit']").val("");
-		$("input[placeholder='Ingredient']").val("");
+			//Reset unit and ingredient inputs
+			$(this).closest("tr").find("input[placeholder='Unit']").val("");
+			$(this).closest("tr").find("input[placeholder='Ingredient']").val("");
+		}
 	}
 }
 
 
-
 //Delete ingredient
-$("tbody").on("click", ".fa-trash-o", deleteIngredient);
-
 function deleteIngredient()
 {
 	$(this).closest("tr").fadeOut(500, function(){
@@ -53,28 +88,22 @@ function deleteIngredient()
 }
 
 
-
-
 //Change table entry to input when clicked
-$("tbody").on("click", "td", function(event){
+function tableEntryToInput(event)
+{
 	var tableText = $(this).html();
 	var inputHtml = "<input type='text' value='" + tableText + "'>"
 	$(this).html(inputHtml);
+	//alert($(this).html());
 
 	//Focus on input after clicked
 	$(this).children().focus();
-});
 
-//Stop input click triggering td click
-$("tbody").on("click", "input", function(event){
 	event.stopPropagation();
-});
+}
 
 
-//Save ingredient to table
-$("tbody").on("change", "input", saveIngredientChanges);
-$("tbody").on("blur", "input", saveIngredientChanges);
-
+//Save ingredient changes to table
 //When changed or clicked off make it a normal table entry again
  function saveIngredientChanges()
  {
